ffc755980d1f999e232bdda6aa9a82eff3f36775
Yusuf Sahin <yusuf@yusufsahin.net>
2026-04-06 22:08:55 +0300

    first commit

---
+++ ch01_dynamictyping/main.py
@@ -0,0 +1,17 @@
+sayi1=10
+sayi2=20
+sayi3=sayi1+sayi2
+print(sayi1)
+print(sayi2)
+print(sayi3)
+
+sayi4=15.6
+sayi5=sayi4+sayi1
+print(sayi4)
+print(sayi5)
+
+print(type(sayi1))
+print(type(sayi2))
+print(type(sayi3))
+print(type(sayi4))
+print(type(sayi5))
